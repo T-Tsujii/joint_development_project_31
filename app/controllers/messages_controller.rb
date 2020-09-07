9e6dcb44f698a307a8b663ed3526cd54ca384feb
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-09-07 23:32:59 +0900

    メッセージの新規投稿機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  # 以下のアクションを追加
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 end
